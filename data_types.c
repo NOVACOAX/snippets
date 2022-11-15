0e527896ee94785cad6da6d1ea686c8f9895eba8
NOVACOAX <58072759+NOVACOAX@users.noreply.github.com>
2022-11-15 07:14:56 +0300

    C data types

---
+++ data_types.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //* Integers (int)
+    int int_value = 3;
+    printf("Integer : %i\n", int_value);
+
+    //* Characters (char)
+    char char_value = 'c';
+    printf("Character : %c\n", char_value);
+
+    //* Float (real)
+    float float_value = 10.54;
+    printf("Float : %.2f\n", float_value);
+
+    //*  Double (real)
+    double double_value = 15.50;
+    printf("Double : %f\n", double_value);
+}
